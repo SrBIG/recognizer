639cc5200e4049ea434354cee56a40a6ca699133
Ivan Biryuchev <ivanbiryuchev@coherentsolutions.com>
2019-10-22 12:41:11 +0300

    Init commit

---
+++ src/main/java/bsuir/ai/recognizer/RecognizerApplication.java
@@ -0,0 +1,19 @@
+package bsuir.ai.recognizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RecognizerApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecognizerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RecognizerApplication.class);
+    }
+}
